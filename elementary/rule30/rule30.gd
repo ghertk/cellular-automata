3d5efc3d04bbde708da4830937e15bebe4f8c334
Ghért Bergler König <ghert_k@hotmail.com>
2023-07-07 23:44:01 -0300

    Change processing style to enable more iterations

--- elementary/rule30/rule30.gd
+++ elementary/rule30/rule30.gd
@@ -1,7 +1,7 @@
 extends Node
 
 
-@export_range(1, 32, 1) var iterations: int = 32
+@export var iterations: int = 32
 @export var blank_cell_color: Color = Color.WHITE
 @export var cell_color: Color = Color.BLACK
 
@@ -12,21 +12,41 @@ func _ready() -> void:
 	var dir = DirAccess.open("user://output-rule-30")
 
 	var size = iterations * 2 - 1
-	var state = 1 << iterations - 1
+	var middle: int = size / 2
+	var values: PackedByteArray = PackedByteArray()
+	for cell in range(size):
+		values.append(cell == middle)
+
+	for r in range(iterations):
+		for l in range(size):
+			var cell_index = r * size + l
+			var left = 0
+			if cell_index != 0:
+				left = values[cell_index - 1]
+			var right = 0
+			if cell_index % size != size - 1:
+				right = values[cell_index + 1]
+			var center = values[cell_index]
+
+			if left == 1:
+				if center == right and center == 0:
+					values.append(1)
+				else:
+					values.append(0)
+			else:
+				if center != left or right != left:
+					values.append(1)
+				else:
+					values.append(0)
 
 	var image = Image.create(size, iterations, false, Image.FORMAT_RGBA8)
 	image.fill(blank_cell_color)
-	var result = PackedInt64Array()
 
-	for i in range(iterations):
-		result.append(state)
-		state = (state >> 1) ^ (state | state << 1)
-
-	for row in range(result.size()):
-		var value = result[row]
-		for column in range(size):
-			if value >> column & 1:
-				image.set_pixel(size - 1 - column, row, cell_color)
+	for i in range(values.size()):
+		if values[i] == 1:
+			var row: int = i / size
+			var column: int = i % size
+			image.set_pixel(column, row, cell_color)
 
 	var file_path = dir.get_current_dir() + "/" + "rule30-{0}x{1}.png".format([image.get_width(), image.get_height()])
 	print("Saving image: " + file_path)
